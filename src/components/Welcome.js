486f7038557c8d0bb60a22c6b658e09647895c04
Merlieb <merline15.mb@gmail.com>
2019-06-07 16:18:04 -0400

    First App on Rect

---
+++ src/components/Welcome.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "../components/CustomButtons/Button.jsx";
+import SectionCarousel from "../views/Components/Sections/SectionCarousel.jsx";
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "../components/Grid/GridContainer.jsx";
+import GridItem from "../components/Grid/GridItem.jsx";
+// import Button from "components/CustomButtons/Button.jsx";
+import HeaderLinks from "../components/Header/HeaderLinks.jsx";
+import Parallax from "../components/Parallax/Parallax.jsx";
+import LandingPage from "../views/LandingPage/LandingPage.jsx";
+import { loadOptions } from "@babel/core";
+class Welcome extends React.Component{
+render() {
+
+    return (       
+        <div>
+        <Button type="button" color="warning"onClick = {()=>this.props.history.push("/reservation-page")}>
+                Reservation
+              </Button>
+              <Button type="button" color="warning" onClick = {()=>this.props.history.push("/landing-page")}>
+                About Us
+              </Button>
+              <Button type="button" color="warning">
+                Reservation
+              </Button>
+            <SectionCarousel />
+            <headerStyle />
+        </div>
+    );
+    }
+
+}
+
+export default Welcome;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.4.0";
+
+// pages for this product
+import Components from "views/Components/Components.jsx";
+import LandingPage from "views/LandingPage/LandingPage.jsx";
+import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
+import ReservationPage from "views/Reservation/ReservationPage.jsx";
+import LoginPage from "views/LoginPage/LoginPage.jsx";
+import Welcome from "./components/Welcome";
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/reservation-page" component={ReservationPage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route exact path="/" component={Welcome} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
